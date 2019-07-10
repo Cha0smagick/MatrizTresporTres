259231b8640bf54ef2dced74874372544d972b4e
Cha0smagick <alekos20@gmail.com>
2019-07-10 00:12:41 -0500

    Array tres por tres jueguito 1.0

---
+++ MatriztresporTres/src/matriztresportres/MatriztresporTres.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package matriztresportres;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Administrador
+ */
+public class MatriztresporTres {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String[][] Array1;
+        Array1 = new String[3][3];
+
+        for (int j = 0; j < Array1.length; j++) {
+            System.out.print("\n");
+            for (int i = 0; i < Array1.length; i++) {
+                String numero = JOptionPane.showInputDialog("Matriz ");
+                Array1[i][j] = numero;
+                System.out.print("\t" + Array1[i][j]);
+            }
+        }
+        String buscar = JOptionPane.showInputDialog("Que numero quiere buscar");
+        for (int i = 0; i < Array1.length; i++) {
+            for (int j = 0; j < Array1.length; j++) {
+                if (buscar.equals(Array1[i][j])) {
+
+                    JOptionPane.showMessageDialog(null, "Numero " + " encontrado en la posicion" + "[" + i + "]" + "[" + j + "]");
+                }
+            }
+        }
+    }
+
+}
