63d73d62fdba9f0c74d7c2a0ab00c6a5c452153a
Cha0smagick <alekos20@gmail.com>
2019-08-23 20:41:30 -0500

    Fixed: eliminated the extra archives.

--- MatriztresporTres/src/matriztresportres/MatriztresporTres.java
+++ MatriztresporTres/src/matriztresportres/MatriztresporTres.java
@@ -13,30 +13,31 @@ import javax.swing.JOptionPane;
  */
 public class MatriztresporTres {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        String[][] Array1;
-        Array1 = new String[3][3];
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		String[][] Array1;
+		Array1 = new String[3][3];
 
-        for (int j = 0; j < Array1.length; j++) {
-            System.out.print("\n");
-            for (String[] Array11 : Array1) {
-                String numero = JOptionPane.showInputDialog("Matriz");
-                Array11[j] = numero;
-                System.out.print("\t" + Array11[j]);
-            }
-        }
-        String buscar = JOptionPane.showInputDialog("Que numero quiere buscar");
-        for (int i = 0; i < Array1.length; i++) {
-            for (int j = 0; j < Array1.length; j++) {
-                if (buscar.equals(Array1[i][j])) {
+		for (int j = 0; j < Array1.length; j++) {
+			System.out.print("\n");
+			for (String[] Array11 : Array1) {
+				String numero = JOptionPane.showInputDialog("Matriz");
+				Array11[j] = numero;
+				System.out.print("\t" + Array11[j]);
+			}
+		}
+		String buscar = JOptionPane.showInputDialog("Que numero quiere buscar");
+		for (int i = 0; i < Array1.length; i++) {
+			for (int j = 0; j < Array1.length; j++) {
+				if (buscar.equals(Array1[i][j])) {
 
-                    JOptionPane.showMessageDialog(null, "Numero " + " encontrado en la posicion" + "[" + j + "]" + "[" + i + "]");
-                }
-            }
-        }
-    }
+					JOptionPane.showMessageDialog(null,
+							"Numero " + " encontrado en la posicion" + "[" + j + "]" + "[" + i + "]");
+				}
+			}
+		}
+	}
 
 }
